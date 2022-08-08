36656b6c410d5a87597ed46b86c88f524c26970a
Sunil <sunilyahoo95@gmail.com>
2022-08-09 01:38:13 +0530

    add basic counter example in block pattern

---
+++ my_flutter_app/lib/CounterBloc.dart
@@ -0,0 +1,35 @@
+
+import 'dart:async';
+
+
+enum CounterAction { Increment, Decrement, Reset}
+
+
+class CounterBloc {
+  
+  int counter;
+
+  final _stateStreamController = StreamController<int>();
+  StreamSink<int> get counterSink => _stateStreamController.sink;
+  Stream<int> get counterStream => _stateStreamController.stream;
+
+
+  final _eventControllerStream = StreamController<CounterAction>();
+  StreamSink<CounterAction> get eventSink => _eventControllerStream.sink;
+  Stream<CounterAction> get eventStream => _eventControllerStream.stream;
+
+
+  CounterBloc() {
+    counter = 0;
+    
+    eventStream.listen((event) {
+      if(event == CounterAction.Increment) counter++;
+      else if(event == CounterAction.Decrement) counter--;
+      else if(event == CounterAction.Reset) counter = 0;
+
+      counterSink.add(counter);
+    });
+    
+  }
+  
+}
--- my_flutter_app/lib/main.dart
+++ my_flutter_app/lib/main.dart
@@ -1,36 +1,89 @@
 import 'package:flutter/material.dart';
+import 'package:my_flutter_app/CounterBloc.dart';
 
 void main() =>
   runApp(MaterialApp(
-    home: Scaffold(
-      appBar: AppBar(
-        title: Text('My First App'),
-        centerTitle: true,
-      ),
-      body: Center(
-        child: Text('Hello Dev'),
-      ),
-      floatingActionButton: FloatingActionButton(
-        child: Text('Click'),
-      ),
-    ),
+    home: CounterWidget()
   ));
 
 
 
-class MyApp extends StatelessWidget {
+class CounterWidget extends StatefulWidget {
+
+  @override
+  _CounterState createState() => _CounterState();
+}
+
+
+class _CounterState extends State<CounterWidget> {
+
+  int counter = 0;
+  final counterBloc = CounterBloc();
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    print("initState counter widget called...");
+  }
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Welcome to Your Home',
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Welcome to Flutter'),
-        ),
-        body: Center(
-          child: Text('Hello World'),
-        ),
+    return  Scaffold(
+      appBar: AppBar(
+        title: Text('My First App'),
+        centerTitle: true,
+        backgroundColor: Colors.red[600],
+      ),
+      body: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          StreamBuilder(
+              stream: counterBloc.counterStream,
+              initialData: 0,
+              builder:  (context, snapshot) {
+                return Text('Counter value is ${snapshot.data}',
+                style: Theme.of(context).textTheme.headline2,);
+              },
+              ),
+        ],
+
+      ),
+
+      floatingActionButton: Row(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: [
+          FloatingActionButton(
+            onPressed: () {
+              counterBloc.eventSink.add(CounterAction.Increment);
+            },
+            tooltip: "Increment",
+            child: Icon(Icons.add),
+            backgroundColor: Colors.red[600],
+          ),
+
+          FloatingActionButton(
+            onPressed: () {
+              counterBloc.eventSink.add(CounterAction.Decrement);
+            },
+            tooltip: "Decrement",
+            child: Icon(Icons.remove),
+            backgroundColor: Colors.red[600],
+          ),
+
+          FloatingActionButton(
+            onPressed: () {
+              counterBloc.eventSink.add(CounterAction.Reset);
+            },
+            tooltip: "Increment",
+            child: Icon(Icons.loop),
+            backgroundColor: Colors.red[600],
+          ),
+
+        ],
       ),
     );
   }
+
 }
